2fe55f1f44baf156cce2b6d5a7347df87b48c4ce
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2023-11-18 01:23:56 -0600

    update dashboard page

--- frontend/src/views/IndexView.jsx
+++ frontend/src/views/IndexView.jsx
@@ -1,11 +1,17 @@
+import { useContext } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import { BoardContext } from "../contexts/BoardContext";
 
 const IndexView = () => {
+  const [state, dispatch] = useContext(BoardContext);
+
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi John, Welcome back 👋
+        {state.user.username
+          ? `Hi ${state.user.username}, Welcome back to EduCompanio!`
+          : "Welcome to EduCompanio!"}
       </Typography>
     </Container>
   );
